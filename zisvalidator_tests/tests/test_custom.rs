56d79b1284ede8c2fb822d36e5ecc7b6db6d8f09
zz <517669936@qq.com>
2021-04-18 16:21:19 +0800

    rewrite tests for custom

--- zisvalidator_tests/tests/test_custom.rs
+++ zisvalidator_tests/tests/test_custom.rs
@@ -3,125 +3,106 @@ use zisvalidator::{error::ValidatorError};
 #[macro_use]
 extern crate lazy_static;
 
-lazy_static! {
-    static ref VALIDATE_STR_FAILED:ValidatorError =  ValidatorError{
-        message:"validate for str fail".to_owned()
-    };
-}
-fn validate_for_str_fail(_:&str) -> Result<(),ValidatorError>{
-    Err(VALIDATE_STR_FAILED.clone())
+#[derive(Validate)]
+struct ValidateStructSucc{
+    #[validate(custom = "validate_succ")]
+    s:String,
 }
 
-fn validate_for_str_succ(_:&str) -> Result<(),ValidatorError>{
+#[derive(Validate,Default)]
+struct ValidateStructFail{
+    #[validate(custom = "validate_fail")]
+    s:String,
+}
+#[derive(Validate,Default)]
+#[validate(custom = "validate_fail")]
+struct ValidateStructTupleFail(String);
+
+#[derive(Validate,Default)]
+#[validate(custom = "validate_succ")]
+struct ValidateStructTupleSucc(String);
+
+#[derive(Validate)]
+enum ValidateEnumSucc{
+    #[validate(custom = "validate_succ")]
+    NewType(String),
+    #[validate(custom = "validate_succ")]
+    Tuple(String,String),
+    Fields{
+        #[validate(custom = "validate_succ")]
+        s:String
+    }
+}
+
+#[derive(Validate)]
+enum ValidateEnumFail{
+    #[validate(custom = "validate_fail")]
+    NewType(String),
+    #[validate(custom = "validate_fail")]
+    Tuple(String,String),
+    Fields{
+        #[validate(custom = "validate_fail")]
+        s:String
+    }
+}
+
+lazy_static! {
+    static ref VALIDATE_FAILED:ValidatorError =  ValidatorError{
+        message:"validate failed".to_owned()
+    };
+    static ref VALIDATE_STRUCT_SUCC:ValidateStructSucc = ValidateStructSucc{
+        s:"succ".to_owned()
+    };
+    static ref VALIDATE_STRUCT_FAIL:ValidateStructFail = ValidateStructFail{
+        s:"fail".to_owned()
+    };
+    static ref VALIDATE_STRUCT_TUPLE_SUCC:ValidateStructTupleSucc = ValidateStructTupleSucc("validate".to_owned());
+    static ref VALIDATE_STRUCT_TUPLE_FAIL:ValidateStructTupleFail = ValidateStructTupleFail("validate".to_owned());
+    static ref VALIDATE_ENUM_NEWTYPE_FAIL:ValidateEnumFail = ValidateEnumFail::NewType("validate".to_owned());
+    static ref VALIDATE_ENUM_NEWTYPE_SUCC:ValidateEnumSucc = ValidateEnumSucc::NewType("validate".to_owned());
+    static ref VALIDATE_ENUM_TUPLE_FAIL:ValidateEnumFail = ValidateEnumFail::Tuple("validate".to_owned(),"validate".to_owned());
+    static ref VALIDATE_ENUM_TUPLE_SUCC:ValidateEnumSucc = ValidateEnumSucc::Tuple("validate".to_owned(),"validate".to_owned());
+    static ref VALIDATE_ENUM_FIELDS_FAIL:ValidateEnumFail = ValidateEnumFail::Fields{s:"validate".to_owned()};
+    static ref VALIDATE_ENUM_FIELDS_SUCC:ValidateEnumSucc = ValidateEnumSucc::Fields{s:"validate".to_owned()};
+
+}
+fn validate_fail(_:&str) -> ValidatorResult{
+    Err(VALIDATE_FAILED.clone())
+}
+
+fn validate_succ(_:&str) -> ValidatorResult{
     Ok(())
 }
+
+type ValidatorResult = Result<(),ValidatorError>;
 
 pub mod test_custom{
     use super::*;
     pub mod test_struct{
         use super::*;
-        pub mod struct_field{
-            use super::*;
-            #[test]
-            fn test_struct_field_fail(){
-                #[derive(Validate)]
-                struct TestStructFieldFail{
-                    #[validate(custom = "validate_for_str_fail")]
-                    field:String,
-                }
-        
-                let test_struct_field_fail = TestStructFieldFail{
-                    field:"test_struct_field_fail".to_owned()
-                };
-                
-                assert_eq!(test_struct_field_fail.validate(),Result::<(),_>::Err(VALIDATE_STR_FAILED.clone()));
-            }
-        
-            #[test]
-            fn test_struct_field_succ(){
-                #[derive(Validate)]
-                struct TestStructFieldSucc{
-                    #[validate(custom = "validate_for_str_succ")]
-                    field:String,
-                }
-                let test_struct_field_succ = TestStructFieldSucc{
-                    field:"test_struct_field_succ".to_owned()
-                };
-                
-                assert_eq!(test_struct_field_succ.validate(),Result::<(),_>::Ok(()));
-            }
+        #[test]
+        fn test_struct_field(){
+            assert_eq!(VALIDATE_STRUCT_FAIL.validate(),ValidatorResult::Err(VALIDATE_FAILED.clone()));
+            assert_eq!(VALIDATE_STRUCT_SUCC.validate(),ValidatorResult::Ok(()));
         }
-        pub mod struct_new_type{
-            use super::*;
-            #[test]
-            fn test_struct_new_type_fail(){
-                #[derive(Validate)]
-                #[validate(custom = "validate_for_str_fail")]
-                struct TestStructNewTypeFail(String);
-        
-                let test_struct_new_type_fail = TestStructNewTypeFail(
-                    "test_struct_new_type_fail".to_owned()
-                );
-                assert_eq!(test_struct_new_type_fail.validate(),Result::<(),_>::Err(VALIDATE_STR_FAILED.clone()));
-            }
-        
-            #[test]
-            fn test_struct_new_type_succ(){
-                #[derive(Validate)]
-                #[validate(custom = "validate_for_str_succ")]
-                struct TestStructNewTypeSucc(
-                    String,
-                );
-                let test_struct_new_type_succ = TestStructNewTypeSucc(
-                    "test_struct_field_succ".to_owned()
-                );
-    
-                assert_eq!(test_struct_new_type_succ.validate(),Result::<(),_>::Ok(()));
-            }
+        #[test]
+        fn test_struct_tuple(){
+            assert_eq!(VALIDATE_STRUCT_TUPLE_FAIL.validate(),ValidatorResult::Err(VALIDATE_FAILED.clone()));
+            assert_eq!(VALIDATE_STRUCT_TUPLE_SUCC.validate(),ValidatorResult::Ok(()));
         }
         
     }
-    
     pub mod test_enum{
         use super::*;
-    
         #[test]
-        fn test_enum_fail(){
-            #[derive(Validate)]
-            enum TestEnum{
-                #[validate(custom = "validate_for_str_fail")]
-                NewType(String),
-                Field{
-                    #[validate(custom = "validate_for_str_fail")]
-                    field:String,
-                }
-            }
-    
-            let test_enum_new_type = TestEnum::NewType("new type".into());
-            assert_eq!(test_enum_new_type.validate(),Err(VALIDATE_STR_FAILED.clone()));
-    
-            let test_enum_field = TestEnum::Field{field:"field".into()};
-            assert_eq!(test_enum_field.validate(),Err(VALIDATE_STR_FAILED.clone()));
+        fn test_enum(){
+            assert_eq!(VALIDATE_ENUM_NEWTYPE_FAIL.validate(),ValidatorResult::Err(VALIDATE_FAILED.clone()));
+            assert_eq!(VALIDATE_ENUM_NEWTYPE_SUCC.validate(),ValidatorResult::Ok(()));
+            assert_eq!(VALIDATE_ENUM_TUPLE_FAIL.validate(),ValidatorResult::Err(VALIDATE_FAILED.clone()));
+            assert_eq!(VALIDATE_ENUM_TUPLE_SUCC.validate(),ValidatorResult::Ok(()));
+            assert_eq!(VALIDATE_ENUM_FIELDS_FAIL.validate(),ValidatorResult::Err(VALIDATE_FAILED.clone()));
+            assert_eq!(VALIDATE_ENUM_FIELDS_SUCC.validate(),ValidatorResult::Ok(()));
         }
-
-        #[test]
-        fn test_enum_succ(){
-            #[derive(Validate)]
-            enum TestEnum{
-                #[validate(custom = "validate_for_str_succ")]
-                NewType(String),
-                Field{
-                    #[validate(custom = "validate_for_str_succ")]
-                    field:String,
-                }
-            }
-    
-            let test_enum_new_type = TestEnum::NewType("new type".into());
-            assert_eq!(test_enum_new_type.validate(),Ok(()));
-    
-            let test_enum_field = TestEnum::Field{field:"field".into()};
-            assert_eq!(test_enum_field.validate(),Ok(()));
-        }
-        
     }
+
 }
