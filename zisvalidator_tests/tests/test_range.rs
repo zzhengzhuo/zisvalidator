e255a2a8acb46e21769db6b5c420289297169978
zz <517669936@qq.com>
2021-04-18 16:21:41 +0800

    rewrite tests for range

--- zisvalidator_tests/tests/test_range.rs
+++ zisvalidator_tests/tests/test_range.rs
@@ -1,108 +1,81 @@
 use zisvalidator::*;
+use zisvalidator::{error::ValidatorError};
+#[macro_use]
+extern crate lazy_static;
 
-pub mod test_custom{
-    use super::*;
-    pub mod test_struct{
-        use super::*;
-        pub mod struct_field{
-            use super::*;
-            #[test]
-            fn test_struct_field_fail(){
-                #[derive(Validate)]
-                struct TestStructFieldFail{
-                    #[validate(range = r#"2.."#)]
-                    field1:i32,
-                }
-        
-                let test_struct_field_fail = TestStructFieldFail{
-                    field1:1,
-                };
-                
-                assert!(test_struct_field_fail.validate().is_err());
-            }
-        
-            #[test]
-            fn test_struct_field_succ(){
-                #[derive(Validate)]
-                struct TestStructFieldSucc{
-                    #[validate(range = r#"2.."#)]
-                    field:i32,
-                }
-                let test_struct_field_succ = TestStructFieldSucc{
-                    field:3
-                };
-                
-                assert!(test_struct_field_succ.validate().is_ok());
-            }
-        }
-        pub mod struct_new_type{
-            use super::*;
-            #[test]
-            fn test_struct_new_type_fail(){
-                #[derive(Validate)]
-                #[validate(range = "2..")]
-                struct TestStructNewTypeFail(i32,u8);
-        
-                let test_struct_new_type_fail = TestStructNewTypeFail(
-                    1,2
-                );
-                assert!(test_struct_new_type_fail.validate().is_err());
-            }
-        
-            #[test]
-            fn test_struct_new_type_succ(){
-                #[derive(Validate)]
-                #[validate(range = "2..")]
-                struct TestStructNewTypeSucc(i32,u8);
-                let test_struct_new_type_succ = TestStructNewTypeSucc(
-                    3,4
-                );
-                assert!(test_struct_new_type_succ.validate().is_ok());
-            }
-        }
-        
+#[derive(Validate)]
+struct ValidateStruct{
+    #[validate(range = "2..")]
+    n:i32,
+}
+
+#[derive(Validate,Default)]
+#[validate(range = "2..")]
+struct ValidateStructTuple(i32);
+
+
+#[derive(Validate)]
+enum ValidateEnum{
+    #[validate(range = "2..")]
+    NewType(i32),
+    #[validate(range = "2..")]
+    Tuple(i32,i32),
+    Fields{
+        #[validate(range = "2..")]
+        n:i32
     }
+}
+type ValidatorResult = Result<(),ValidatorError>;
+
+lazy_static! {
+    static ref VALIDATE_STRUCT_SUCC:ValidateStruct = ValidateStruct{
+        n:3
+    };
+    static ref VALIDATE_STRUCT_FAIL:ValidateStruct = ValidateStruct{
+        n:1
+    };
+    static ref VALIDATE_STRUCT_TUPLE_SUCC:ValidateStructTuple = ValidateStructTuple(3);
+    static ref VALIDATE_STRUCT_TUPLE_FAIL:ValidateStructTuple = ValidateStructTuple(1);
+    static ref VALIDATE_ENUM_NEWTYPE_FAIL:ValidateEnum = ValidateEnum::NewType(1);
+    static ref VALIDATE_ENUM_NEWTYPE_SUCC:ValidateEnum = ValidateEnum::NewType(3);
+    static ref VALIDATE_ENUM_TUPLE_FAIL:ValidateEnum = ValidateEnum::Tuple(3,1);
+    static ref VALIDATE_ENUM_TUPLE_SUCC:ValidateEnum = ValidateEnum::Tuple(3,3);
+    static ref VALIDATE_ENUM_FIELDS_FAIL:ValidateEnum = ValidateEnum::Fields{n:1};
+    static ref VALIDATE_ENUM_FIELDS_SUCC:ValidateEnum = ValidateEnum::Fields{n:3};
     
+}
+fn validator_error<T:std::fmt::Debug>(field:&str,attr:&str,value:T) -> ValidatorError{
+    ValidatorError{
+        message:validator_error!(field,attr,value)
+    }
+}
+#[cfg(test)]
+mod test_range{
+    use super::*;
+    mod test_struct{
+        use super::*;
+        #[test]
+        fn test_struct_field(){
+            assert_eq!(VALIDATE_STRUCT_FAIL.validate(),ValidatorResult::Err(validator_error("n","range",2..)));
+            assert_eq!(VALIDATE_STRUCT_SUCC.validate(),ValidatorResult::Ok(()));
+        }
+        #[test]
+        fn test_struct_tuple(){
+            assert_eq!(VALIDATE_STRUCT_TUPLE_FAIL.validate(),ValidatorResult::Err(validator_error("ValidateStructTuple","range",2..)));
+            assert_eq!(VALIDATE_STRUCT_TUPLE_SUCC.validate(),ValidatorResult::Ok(()));
+        }
+    }
     pub mod test_enum{
         use super::*;
-    
         #[test]
-        fn test_enum_fail(){
-            #[derive(Validate)]
-            enum TestEnum{
-                #[validate(range = "2..")]
-                NewType(i32),
-                Field{
-                    #[validate(range = "2..")]
-                    field:i64,
-                }
-            }
-    
-            let test_enum_new_type = TestEnum::NewType(1);
-            assert!(test_enum_new_type.validate().is_err());
-    
-            let test_enum_field = TestEnum::Field{field:1};
-            assert!(test_enum_field.validate().is_err());
+        fn test_enum(){
+            assert_eq!(VALIDATE_ENUM_NEWTYPE_FAIL.validate(),ValidatorResult::Err(validator_error("NewType","range",2..)));
+            assert_eq!(VALIDATE_ENUM_NEWTYPE_SUCC.validate(),ValidatorResult::Ok(()));
+            assert_eq!(VALIDATE_ENUM_TUPLE_FAIL.validate(),ValidatorResult::Err(validator_error("Tuple","range",2..)));
+            assert_eq!(VALIDATE_ENUM_TUPLE_SUCC.validate(),ValidatorResult::Ok(()));
+            assert_eq!(VALIDATE_ENUM_FIELDS_FAIL.validate(),ValidatorResult::Err(validator_error("n","range",2..)));
+            assert_eq!(VALIDATE_ENUM_FIELDS_SUCC.validate(),ValidatorResult::Ok(()));
         }
-
-        #[test]
-        fn test_enum_succ(){
-            #[derive(Validate)]
-            enum TestEnum{
-                #[validate(range = "2..")]
-                NewType(i32),
-                Field{
-                    #[validate(range = "2..")]
-                    field:i64,
-                }
-            }
-    
-            let test_enum_new_type = TestEnum::NewType(3);
-            assert!(test_enum_new_type.validate().is_ok());
-    
-            let test_enum_field = TestEnum::Field{field:3};
-            assert!(test_enum_field.validate().is_ok());
-        }
-        
     }
+        
 }
